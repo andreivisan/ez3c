e99f11d583d14edf720d0931fd2895fd51bc04fc
Andrei Visan <andrei.m.visan@gmail.com>
2026-06-13 19:27:17 +0300

    Small progress

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use std::env;
+use std::{env, fs};
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // consider if let Some here instead
     match env::home_dir() {
         Some(home_path) => {
             let path = home_path.join(".claude").join("projects");
             println!("Your Claude directory, probably: {}", path.display());
+            for entry in fs::read_dir(path)? {
+                let entry = entry?;
+                let path = entry.path();
+                println!("{}", path.display());
+            }
         }
         None => println!("Impossible to get your home dir"),
     }
+    Ok(())
 }
